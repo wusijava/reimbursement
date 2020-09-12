0d977ce4f2b7daeb692e442cfb78664b8e405147
wusi <513936307@qq.com>
2020-09-12 17:09:09 +0800

    测试生成不同颜色二维码

--- src/main/java/com/wusi/reimbursement/Main.java
+++ src/main/java/com/wusi/reimbursement/Main.java
@@ -2,16 +2,20 @@ package com.wusi.reimbursement;
 
 import com.wusi.reimbursement.entity.*;
 import com.wusi.reimbursement.generator.CodeGenerator;
+import com.wusi.reimbursement.utils.QrUtil;
+
+import java.awt.image.BufferedImage;
 
 
 public class Main {
 
-    public static void main(String[] args) {
-        String basePack = Main.class.getPackage().getName();
+    public static void main(String[] args) throws Exception {
+      /*  String basePack = Main.class.getPackage().getName();
         CodeGenerator codeGenerator = new CodeGenerator();
-        codeGenerator.generateMybatisXml(basePack, Reimbursement.class);
+        codeGenerator.generateMybatisXml(basePack, Reimbursement.class);*/
        // codeGenerator.generateDao(basePack, Reimbursement.class);
       // codeGenerator.generateService(basePack, Reimbursement.class);
-
+        BufferedImage grayQuickMarkImage = QrUtil.buildGrayQuickMarkImage("http://49.233.192.222:8082/#/entrance/select-action");
+        QrUtil.writeToLocal("D:/灰码.jpg",grayQuickMarkImage);
     }
 }
