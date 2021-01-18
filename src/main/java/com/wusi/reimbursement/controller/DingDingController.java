a132723e8f0f45aab7b7bf74e863681a09bdc544
wusi <513936307@qq.com>
2021-01-18 14:49:15 +0800

    解决加入钉钉机器人本地jar无法自动发布

---
+++ src/main/java/com/wusi/reimbursement/controller/DingDingController.java
@@ -0,0 +1,23 @@
+package com.wusi.reimbursement.controller;
+
+import com.wusi.reimbursement.common.Response;
+import com.wusi.reimbursement.utils.DingDingTalkUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ Description   :  钉钉机器人群发器
+ * @ Author        :  wusi
+ * @ CreateDate    :  2021/1/18$ 14:32$
+ */
+@RestController
+@Slf4j
+public class DingDingController {
+    @RequestMapping(value = "sendDingDingMessage")
+    public Response sendDingDingMessage(String content) throws Exception {
+        DingDingTalkUtils.sendDingDingMsg(content);
+        return Response.ok("群发成功!");
+    }
+
+}
