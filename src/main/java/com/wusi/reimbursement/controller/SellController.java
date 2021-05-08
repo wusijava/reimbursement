fc187e5d230cc0e957374ca071d53606484239dc
wusi <513936307@qq.com>
2021-05-08 14:17:07 +0800

    排班表模板

--- src/main/java/com/wusi/reimbursement/controller/SellController.java
+++ src/main/java/com/wusi/reimbursement/controller/SellController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.ss.util.RegionUtil;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -146,4 +147,79 @@ public class SellController {
         CellRangeAddress bigOrderRegion = new CellRangeAddress( start+2, end+1 , index, index);
         sheet.addMergedRegion(bigOrderRegion);
     }
+    @GetMapping(value = "getBan")
+    public void getBan(String date, HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        Date d = null;
+        DateFormat sdf = new SimpleDateFormat(DateUtil.PATTERN_YYYY_MM_DD);
+        try {
+            d = sdf.parse(date+"-01");
+        } catch (Exception e) {
+            d = new Date();
+            date = sdf.format(d);
+        }
+        File f = new File("/home/file/ban.xlsx");
+        //File f = new File("D:\\excel\\ban.xlsx");
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(f);
+            Workbook wb = new XSSFWorkbook(fis);
+            Sheet sheet = wb.getSheetAt(0);
+            String str=date.substring(5, 7).startsWith("0")?date.substring(5, 7).replace("0", ""):date.substring(5, 7);
+            wb.setSheetName(0,str+"月班表" );
+            Calendar one = Calendar.getInstance();
+            one.setTime(d);
+            one.set(Calendar.DAY_OF_MONTH, one.getActualMinimum(Calendar.DAY_OF_MONTH));//获取第一天
+            Date oneDate=one.getTime();
+            //最后一天
+            Calendar last = Calendar.getInstance();
+            last.setTime(d);
+            last.set(Calendar.DAY_OF_MONTH, last.getActualMaximum(Calendar.DAY_OF_MONTH));//获取最后一天
+            Date lastDate=last.getTime();
+            long days = DateUtil.betweenDays(oneDate, lastDate);
+            Row row;
+            String[] weekDays = {"周日", "周一", "周二", "周三", "周四", "周五", "周六"};
+            for (int i =0;i<days+1;i++){
+                int w = one.get(Calendar.DAY_OF_WEEK) - 1;
+                if (w < 0) {
+                    w = 0;
+                }
+                CellStyle style=wb.createCellStyle();
+                /*style.setFillBackgroundColor((IndexedColors.SEA_GREEN.getIndex()));
+                style.setFillPattern(XSSFCellStyle.FINE_DOTS );*/
+                style.setBorderBottom(new Short("1"));
+                style.setBorderTop(new Short("1"));
+                style.setBorderRight(new Short("1"));
+                style.setBorderLeft(new Short("1"));
+                row=sheet.getRow(0);
+                row.createCell(0).setCellValue(str+"月份");
+                row.createCell(i+1).setCellValue(i+1);
+                row.getCell(i+1).setCellStyle(style);
+                row=sheet.getRow(1);
+                row.createCell(i+1).setCellValue(weekDays[w]);
+                row.getCell(i+1).setCellStyle(style);
+                one.add(Calendar.DAY_OF_MONTH, 1);
+            }
+            String file_name = "排班计划-" +date +".xlsx";
+            response.setContentType("application/vnd.ms-excel;charset=utf-8");
+            response.addHeader("Content-Disposition", "attachment;filename=" + new String(file_name.getBytes("GBK"), "ISO8859-1"));
+            response.setCharacterEncoding("utf-8");
+            OutputStream outputStream = response.getOutputStream();
+            wb.write(outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
 }
